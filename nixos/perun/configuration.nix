2ded6bae4a153b0c5153c37df2dc30f1fbf2bac0
WJKPK <krupskiwojciech@gmail.com>
2025-02-13 22:12:56 +0100

    Regenerate lock & substituers refactor

--- nixos/perun/configuration.nix
+++ nixos/perun/configuration.nix
@@ -11,16 +11,6 @@
     users.kruppenfield = import ../../home-manager/perun.nix;
   };
 
-  nix.settings = {
-    substituters = [ "https://cuda-maintainers.cachix.org"
-                     "https://hyprland.cachix.org"
-                     ];
-    trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
-
   users.users.kruppenfield = {
     extraGroups = [ "docker" "networkmanager" "wheel" "libvirtd" ];
   };
