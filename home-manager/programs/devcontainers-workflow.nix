ae90ed5cd0db00546a15db3ae2dda88b20581c10
WJKPK <krupskiwojciech@gmail.com>
2025-09-06 00:43:53 +0200

    Update devcontainer workflow

--- home-manager/programs/devcontainers-workflow.nix
+++ home-manager/programs/devcontainers-workflow.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}:
 let
   devc-nix-infect = pkgs.writeShellScriptBin "devc-nix-infect" ''
-    devcontainer up --workspace-folder . && devcontainer exec --workspace-folder . -- bash -c "echo 'sandbox = false' > /tmp/nix.conf && \
-        curl -L https://nixos.org/nix/install | bash -s -- --no-daemon --nix-extra-conf-file /tmp/nix.conf && \
-        . \$HOME/.nix-profile/etc/profile.d/nix.sh"
+    devcontainer up --workspace-folder . && devcontainer exec --workspace-folder . -- bash -c "mkdir -p \$HOME/.config/nix && echo -e 'sandbox = false\nexperimental-features = nix-command flakes' > \$HOME/.config/nix/nix.conf && \
+    curl -L https://nixos.org/nix/install | bash -s -- --no-daemon --nix-extra-conf-file \$HOME/.config/nix/nix.conf && \
+    . \$HOME/.nix-profile/etc/profile.d/nix.sh && . \$HOME/.nix-profile/etc/profile.d/nix-daemon.sh"
   '';
 
   devc-shell = pkgs.writeShellScriptBin "devc-shell" ''
