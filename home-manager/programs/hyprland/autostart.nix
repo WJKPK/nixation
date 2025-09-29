5dadd6f6fa6e9b1813d59e6b860a0aa224327cc1
Wojciech Krupski <wojciech.krupski@grinn-global.com>
2025-09-29 14:44:11 +0200

    Move to omarchy-like hyprland config

---
+++ home-manager/programs/hyprland/autostart.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "dunst"
+      "dbus-update-activation-environment --systemd HYPRLAND_INSTANCE_SIGNATURE"
+      "hyprctl setcursor Bibata-Modern-Classic 18"
+      "systemctl --user start hyprpolkitagent"
+      "wl-clip-persist --clipboard regular & clipse -listen"
+    ];
+
+    exec = [
+      "hyprshade auto"
+    ];
+  };
+}
---
+++ home-manager/programs/hyprland/input.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  ...
+}: {
+  wayland.windowManager.hyprland.settings = {
+    # Environment variables
+    # https://wiki.hyprland.org/Configuring/Variables/#input
+    input = lib.mkDefault {
+      kb_layout = "pl";
+      # kb_variant =
+      # kb_model =
+      kb_options = "compose:caps";
+      # kb_rules =
+
+      follow_mouse = 0;
+
+      sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+
+      touchpad = {
+        natural_scroll = false;
+      };
+    };
+
+    # https://wiki.hyprland.org/Configuring/Variables/#gestures
+    gestures = lib.mkDefault {
+      workspace_swipe = false;
+    };
+  };
+}
